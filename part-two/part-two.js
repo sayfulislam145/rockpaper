74ee743e9b326ed47f86602d2b761075612f075c
sayful-seven <sayfulislam145@gmail.com>
2023-04-29 01:47:30 +0900

    first commit

---
+++ part-two/part-two.js
@@ -0,0 +1,19 @@
+
+
+const totalOrder = 10 + 8 * 3 + 5;
+console.log('a', totalOrder)
+console.log('b', (10 + 8 * 3 + 5) * 3 / 3)
+console.log('c', 18.50 + 7.50 * 2)
+console.log('d', (18.50 + 7.50 * 2) * .1)
+console.log('e', (18.50 + 7.50 * 2) * .2)
+console.log('f', (1899 + 2095 + 799) / 100)
+console.log('g', 18.99 + 20.95 + 7.99 + 4.99)
+console.log('h', Math.round((1899 + 2095 + 799 + 499) * .1) / 100)
+console.log('i', Math.round(5292 + 529) / 100)
+console.log('j', Math.floor(2.9))
+console.log('k', Math.ceil(2.9))
+console.log('l', (25 * 9 / 5) + 32)
+console.log('m', (86 - 32) * 5 / 9)
+console.log('n', (-5 * 9 / 5) + 32)
+console.log()
+console.log()
